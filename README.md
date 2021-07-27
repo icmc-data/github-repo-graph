026e16ab122dea3384303043a0b7a3d86cdfcf0b
Alvaro-dev <alvarojoselopes@usp.br>
2021-07-27 13:48:49 -0300

    update README.md

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Transformar o github num grafo, realizar processos de aprendizado e exploração
 .
 │
 ├── dataset/
+|    ├── sqlite/        <- sqlite github database
 |    └── json/          <- network as json files
 └── make/
-    ├── features/       <- obtenção das features
-    └── network/        <- obtenção da rede(dependências e dependentes)
+    ├── features/       <- features getter
+    └── network/        <- network getter (dependencies)
 ```
 ## Step 0 - Installing requirements
 
@@ -16,4 +17,42 @@ Transformar o github num grafo, realizar processos de aprendizado e exploração
 2. Create a virtual environment ([`venv`](https://docs.python.org/3/library/venv.html))
 3. Activate your environment: `$ source [ENVIRONMENT_NAME]/bin/activate`
 4. Install dependencies: `$ pip install -r requirements.txt`
-5. Have a Github personal token (generate here: https://github.com/settings/tokens) to insert on .env
+5. Have a Github personal token (generate [here](https://github.com/settings/tokens)) to insert on .env
+
+## Using the repo
+
+There are mainly 2 Python Scripts, one network visualization using D3.js and some EDA on a jupyter notebook.
+
+### Fetching dependencies of a repository to a JSON file
+
+Run this command:
+
+```
+$ python3 getGithubNetwork.py -r [REPO_NAME] -o [JSON_FILENAME] -d [DEPTH]
+```
+
+This script uses GitHub GraphQL API and a Depth Limited Search(DLS) to fetch dependencies until reach the depth limit. 
+
+If you give `--depth 0`, then the script will try to find all dependencies, as far down as they go.
+
+The Json file will be availabe at `dataset/json/` directory.
+
+### Converting network from github-to-sqlite database to a JSON file
+
+Initially you need to buil a database using **[github-to-sqlite](https://github.com/dogsheep/github-to-sqlite)**. Originally only the `scrape-dependents` script is available in the original repository. Our script to get dependencies was added on this forked [repository](https://github.com/AlvaroJoseLopes/github-to-sqlite)
+
+Run this command:
+
+```
+$ python3 getNetworkFromSqlite.py -db [DATABASE_NAME] -s [MINIMUM_STARS] -o [JSON_FILENAME]
+```
+
+This script fetch a network from **dependents** table and convert it to a JSON file. The `--stars` parameter indicates the minimum number of stars that a repository must have in order to be added to the network.
+
+If you five `--stars 0`, then the script will add all repositories to the network.
+
+The Json file will be availabe at `dataset/json/` directory.
+
+### D3.js Visualization
+
+D3.js was used to view a sample of the network. You can see [here](https://bl.ocks.org/AlvaroJoseLopes/a93e54ea8e61380d3cba9ea52a9e6e08) or opening `index.html` on a local host.
--- getNetworkFromSqlite.py
+++ getNetworkFromSqlite.py
@@ -3,7 +3,7 @@ import json
 import argparse
 
 DEPENDENTS_QUERY = '''SELECT repo, dependent FROM dependents'''
-FETCH_FULL_NAME_QUERY = '''SELECT full_name FROM repos WHERE id = %s'''
+FETCH_FULL_NAME_QUERY = '''SELECT full_name, stargazers_count FROM repos WHERE id = %s'''
 
 class DependentsTable:
 	def __init__(self, db):
@@ -12,20 +12,24 @@ class DependentsTable:
 		self.repos = {}
 		self.json_data = {"nodes": [], "links": []} 
 	
-	def to_json(self, out):
+	def to_json(self, out, minimum = 0):
 		dependents = self.__fetchAllDependents()
 
 		for repo_id, dependent_id in dependents:
-			if repo_id not in self.repos:
-				self.__fetchRepoFullName(repo_id) 
-
-			if dependent_id not in self.repos:
-				self.__fetchRepoFullName(dependent_id)
 			
-			self.json_data['links'].append(
-			{'source': self.repos[repo_id],
-			 'target': self.repos[dependent_id]}
-			)
+			flag_repo = True
+			if repo_id not in self.repos:
+				flag_repo = self.__fetchRepoFullName(repo_id, minimum) 
+
+			flag_dependent = True
+			if dependent_id not in self.repos:
+				flag_dependent = self.__fetchRepoFullName(dependent_id, minimum)
+			
+			if flag_dependent and flag_repo:
+				self.json_data['links'].append(
+				{'source': self.repos[repo_id],
+				'target': self.repos[dependent_id]}
+				)
 
 		jsonGraph = open(out, "w")
 		json.dump(self.json_data, jsonGraph, indent=4)
@@ -36,13 +40,18 @@ class DependentsTable:
 		self.cursor.execute(DEPENDENTS_QUERY)
 		return self.cursor.fetchall()
 
-	def __fetchRepoFullName(self, id):
+	def __fetchRepoFullName(self, id, minimum) -> bool:
 		self.cursor.execute(FETCH_FULL_NAME_QUERY % (id))
-		repo_full_name = self.cursor.fetchone()[0]
-		self.json_data["nodes"].append(
-			{"id": repo_full_name}
-		)
-		self.repos[id] = repo_full_name
+		repo_full_name, stars = self.cursor.fetchone()
+
+		if stars >= minimum:
+			self.json_data["nodes"].append(
+				{"id": repo_full_name}
+			)
+			self.repos[id] = repo_full_name
+			return True
+		else:
+			return False
 	
 	def __close(self):
 		if self.connection:
@@ -59,6 +68,7 @@ class DependentsTable:
 def setup():
 	parser = argparse.ArgumentParser(description = 'Get Github network json file, from a given sqlite database(github-to-sqlite)')
 	parser.add_argument('-db', '--database', help = 'Sqlite database name', required=True)
+	parser.add_argument('-s', '--stars', help = 'Minimum star count. 0 to add all repos to the network', required=True, type=int)
 	parser.add_argument('-o', '--out', help = 'Destination json filename', required=True)
 	args = parser.parse_args()
 	
@@ -69,9 +79,10 @@ def main():
 	args = setup()
 	db_name = 'dataset/sqlite/' + args.database
 	json_filename = 'dataset/json/' + args.out
+	minimum = args.stars
 
 	with DependentsTable(db_name) as db:
-		db.to_json(json_filename)
+		db.to_json(json_filename, minimum)
 
 
 
